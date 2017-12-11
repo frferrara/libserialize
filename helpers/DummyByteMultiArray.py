a9058f8be8f5bfb756bb814b03bdfdfacb3126e3
Francesco Ferrara <francesco.ferrara@helbling.ch>
2017-12-11 10:38:54 +0100

    Library works with dummy objects

--- helpers/DummyByteMultiArray.py
+++ helpers/DummyByteMultiArray.py
@@ -4,4 +4,17 @@ class DummyMultiArrayDimension(object):
         self.size = size
         self.stride = stride
 
+
+class DummyMultiArrayLayout(object):
+    def __init__(self, data_offset):
+        self.dim = []
+        self.data_offset = data_offset
+
+    def add(self, dim):
+        self.dim.append(dim)
+
+
 class DummyByteMultiArray(object):
+    def __init__(self, layout, data):
+        self.layout = layout
+        self.data = data
--- libserialize.py
+++ libserialize.py
@@ -2,10 +2,11 @@
 This file contains the serializer library for the fleet communication project.
 """
 
+
 # Import modules
 from proto.Any_pb2            import Any
 from proto.ByteMultiArray_pb2 import ByteMultiArray
-from proto.ByteMultiArray_pb2 import MultiArrayLayout
+from copy                     import copy
 
 
 def serialize_bma(ros_msg):
@@ -17,24 +18,22 @@ def serialize_bma(ros_msg):
     - bma_data: serialized data (see ByteMultiArray.proto)
     """
     # Create the Serialization objects
-    multi_array_layout = MultiArrayLayout()
-    byte_multi_array = ByteMultiArray()
+    bma = ByteMultiArray()
 
     # Loop through the dimensions in the layout
     for dim in ros_msg.layout.dim:
-        ma_dim = multi_array_layout.dim.add()
+        ma_dim = bma.layout.dim.add()
         ma_dim.label = dim.label
         ma_dim.size = dim.size
         ma_dim.stride = dim.stride
-    
+
     # Set the data offset
-    multi_array_layout.data_offset = ros_msg.layout.data_offset
+    bma.layout.data_offset = ros_msg.layout.data_offset
 
-    # Set the ByteMultiArray
-    byte_multi_array.layout = multi_array_layout
-    byte_multi_array.data = ros_msg.data
+    # Set the data
+    bma.data = ros_msg.data
 
-    bma_data = byte_multi_array.SerializeToString()
+    bma_data = bma.SerializeToString()
     return bma_data
 
 def serialize_any(name, bma_data):
@@ -58,7 +57,6 @@ def serialize_any(name, bma_data):
 
     return any_data
 
-
 def serialize(name, ros_msg):
     """
     This function is used to serialize the data using a two step
@@ -69,28 +67,69 @@ def serialize(name, ros_msg):
     Outputs:
     - any_data: serialized data (byte string, resulting from Any.proto)
     """
-    # Perform the serialization
+    # Perform the seros_msgrialization
     bma_data = serialize_bma(ros_msg)
     any_data = serialize_any(name, bma_data)
 
     return any_data
 
-def deserialize(data):
+def parse_any(data):
     """
-    This function is used to deserialize the data using a two step
-    serialization.
+    This function is used to parse the Any data.
     Inputs:
     - data: serialized data (byte string, resulting from Any.proto)
     Outputs:
-    - name:    name of the serialization (needed for Any.proto)
-    - ros_msg: ROS message to be serialized
+    - name:     name of the serialization (needed for Any.proto)
+    - bma_data: ByteMultiArray data (see ByteMultiArray.proto)
     """
-    name = None
-    ros_msg = None
+    # Instantiate the proto object
+    any_proto = Any()
+
+    # Parse the data
+    any_proto.ParseFromString(data)
+    name = any_proto.type_url
+    bma_data = any_proto.value
+
+    return name, bma_data
+
+def parse_bma(bma_data, mad, ros_msg):
+    """
+    This function is used to parse the ByteMultiArray data.
+    Inputs:
+    - bma_data: ByteMultiArray data (see ByteMultiArray.proto)
+    - mad:      Empty MultiArrayDimension object for filling of the ROS message
+    - ros_msg:  Empty ROS message
+    Outputs:
+    - ros_msg: ROS message ByteMultiArray
+    """
+    # Instantiate the proto object
+    bma = ByteMultiArray()
+
+    # Parse the data
+    bma.ParseFromString(bma_data)
+
+    # Populate the ROS message
+    for dim in bma.layout.dim:
+        mad.label = dim.label
+        mad.size = dim.size
+        mad.stride = dim.stride
+        ros_msg.layout.dim.append(copy(mad))
+    ros_msg.layout.data_offset = bma.layout.data_offset
+    ros_msg.data = bma.data
+
+    return ros_msg
+
+def parse(data, mad, ros_msg):
+    """
+    This function is used to parse the data using a two step serialization.
+    Inputs:
+    - data:    serialized data (byte string, resulting from Any.proto)
+    - mad:     Empty MultiArrayDimension object for filling of the ROS message
+    - ros_msg: Empty ROS message
+    Outputs:
+    - name:    name of the serialization (needed for Any.proto)
+    - ros_msg: ROS message with the parsed data
+    """
+    name, bma_data = parse_any(data)
+    ros_msg = parse_bma(bma_data, mad, ros_msg)
     return name, ros_msg
-
-
-# Basic testing
-if __name__ == "__main__":
-    serialize("bla", "bla")
-    deserialize("bla")
