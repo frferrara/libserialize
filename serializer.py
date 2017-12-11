59d0723bd823f3ef03dd93623d4af376bf4e532e
Francesco Ferrara <francesco.ferrara@helbling.ch>
2017-12-11 07:56:18 +0100

    First definitions

---
+++ serializer.py
@@ -0,0 +1,18 @@
+"""
+This file contains the serializer class for the fleet communication project.
+"""
+
+# Import modules
+
+
+
+class Serializer(object):
+    """
+    The serializer class implements methods to serialize and de-serialize
+    messages in duckietown that are sent and received to/from other duckiebots.
+    """
+
+    def __init__(self):
+        """
+        Constructor of the serializer object.
+        """
